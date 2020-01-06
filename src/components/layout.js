de0fd0644ca16efe888b11951964daf3bcd98df2
Yvonne Nkr <yvonnkr86@gmail.com>
2020-01-06 18:37:34 +0000

    added the footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./bootstrap.min.css";
 import "./layout.css";
 import Navbar from "./Globals/Navbar";
+import Footer from "./Globals/Footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   );
 };
