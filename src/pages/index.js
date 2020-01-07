0f8b7aa683e4fa82ab2f635c68f3ddbb3f5e06b6
Yvonne Nkr <yvonnkr86@gmail.com>
2020-01-07 00:50:40 +0000

    added Product & Products components plus queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo";
 import BackgroundSection from "./../components/Globals/BackgroundSection";
 import Info from "./../components/Home/Info";
 import Menu from "./../components/Home/Menu";
+import Products from "../components/Home/Products";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -17,6 +18,7 @@ const IndexPage = ({ data }) => (
     />
     <Info />
     <Menu items={data.menu} />
+    <Products />
   </Layout>
 );
 
